1705f83deccc2a88478a26c118979b2148e8fb64
Mr.喜 <863450594@qq.com>
2024-10-31 14:06:09 +0800

    更新版本号

--- me.cqp.luohuaming.AiPictureDetector.Core/Export/CQEventExport.cs
+++ me.cqp.luohuaming.AiPictureDetector.Core/Export/CQEventExport.cs
@@ -64,7 +64,7 @@ namespace me.cqp.luohuaming.AiPictureDetector.App.Export
 			// 反射获取 AppData 实例	
 			Type appDataType = typeof (AppData);	
 			// 注册一个 CQApi 实例	
-			AppInfo appInfo = new AppInfo ("me.cqp.luohuaming.AiPictureDetector", 1, 9, "水银抗癌机", "1.1.0", 1, "落花茗", "癌图检测", authCode);	
+			AppInfo appInfo = new AppInfo ("me.cqp.luohuaming.AiPictureDetector", 1, 9, "水银抗癌机", "1.2.0", 1, "落花茗", "癌图检测", authCode);	
 			appDataType.GetRuntimeProperty ("CQApi").GetSetMethod (true).Invoke (null, new object[] { new CQApi (appInfo) });	
 			AppData.UnityContainer.RegisterInstance<CQApi> ("me.cqp.luohuaming.AiPictureDetector", AppData.CQApi);	
 			// 向容器注册一个 CQLog 实例	
